4df06b1ec871bda7f474c1b9022bae97af0c23d7
JonyAhmed <piistech@gmail.com>
2021-09-29 13:29:56 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // backgroundColor: Color(0xfbfafb),
+        backgroundColor: Color(0xfbfafb),
         appBar: AppBar(
           title: Text(
             'San Francisco',
